756d687103018d25c19df838095b69937db2e6c7
ReaperOAK <oaak78692@gmail.com>
2024-12-29 16:00:15 +0530

    check

--- src/data/eggprices.js
+++ src/data/eggprices.js
@@ -1,177 +1,212 @@
 const eggprices =   {
     "headers": [
         "City",
+        "State",
         "Date",
         "Rate"
     ],
     "rows": [
         {
             "city": "Ahmedabad",
+            "state": "Gujarat",
             "date": "2024-12-29",
             "rate": "5.90"
         },
         {
             "city": "Ajmer",
+            "state": "Rajasthan",
             "date": "2024-12-29",
             "rate": "5.80"
         },
         {
             "city": "Barwala",
+            "state": "Haryana",
             "date": "2024-12-29",
             "rate": "5.95"
         },
         {
             "city": "Bengaluru (CC)",
+            "state": "Karnataka",
             "date": "2024-12-29",
             "rate": "5.80"
         },
         {
             "city": "Brahmapur (OD)",
+            "state": "Odisha",
             "date": "2024-12-29",
             "rate": "5.75"
         },
         {
             "city": "Chennai (CC)",
+            "state": "Tamil Nadu",
             "date": "2024-12-29",
             "rate": "6.10"
         },
         {
             "city": "Chittoor",
+            "state": "Andhra Pradesh",
             "date": "2024-12-29",
             "rate": "6.03"
         },
         {
             "city": "Delhi (CC)",
+            "state": "Delhi",
             "date": "2024-12-29",
             "rate": "6.40"
         },
         {
             "city": "E.Godavari",
+            "state": "Andhra Pradesh",
             "date": "2024-12-29",
             "rate": "5.65"
         },
         {
             "city": "Hospet",
+            "state": "Karnataka",
             "date": "2024-12-29",
             "rate": "5.30"
         },
         {
             "city": "Hyderabad",
+            "state": "Telangana",
             "date": "2024-12-29",
             "rate": "5.50"
         },
         {
             "city": "Jabalpur",
+            "state": "Madhya Pradesh",
             "date": "2024-12-29",
             "rate": "5.80"
         },
         {
             "city": "Kolkata (WB)",
+            "state": "West Bengal",
             "date": "2024-12-29",
             "rate": "6.10"
         },
         {
             "city": "Ludhiana",
+            "state": "Punjab",
             "date": "2024-12-29",
             "rate": "5.97"
         },
         {
             "city": "Mumbai (CC)",
+            "state": "Maharashtra",
             "date": "2024-12-29",
             "rate": "6.20"
         },
         {
             "city": "Mysuru",
+            "state": "Karnataka",
             "date": "2024-12-29",
             "rate": "5.80"
         },
         {
             "city": "Namakkal",
+            "state": "Tamil Nadu",
             "date": "2024-12-29",
             "rate": "5.30"
         },
         {
             "city": "Pune",
+            "state": "Maharashtra",
             "date": "2024-12-29",
             "rate": "6.35"
         },
         {
             "city": "Raipur",
+            "state": "Chhattisgarh",
             "date": "2024-12-29",
             "rate": "5.70"
         },
         {
             "city": "Surat",
+            "state": "Gujarat",
             "date": "2024-12-29",
             "rate": "6.00"
         },
         {
             "city": "Vijayawada",
+            "state": "Andhra Pradesh",
             "date": "2024-12-29",
             "rate": "5.65"
         },
         {
             "city": "Vizag",
+            "state": "Andhra Pradesh",
             "date": "2024-12-29",
             "rate": "6.00"
         },
         {
             "city": "W.Godavari",
+            "state": "Andhra Pradesh",
             "date": "2024-12-29",
             "rate": "5.65"
         },
         {
             "city": "Warangal",
+            "state": "Telangana",
             "date": "2024-12-29",
             "rate": "5.87"
         },
         {
             "city": "Allahabad (CC)",
+            "state": "Uttar Pradesh",
             "date": "2024-12-29",
             "rate": "6.29"
         },
         {
             "city": "Bhopal",
+            "state": "Madhya Pradesh",
             "date": "2024-12-29",
             "rate": "6.10"
         },
         {
             "city": "Indore (CC)",
+            "state": "Madhya Pradesh",
             "date": "2024-12-29",
             "rate": "5.70"
         },
         {
             "city": "Kanpur (CC)",
+            "state": "Uttar Pradesh",
             "date": "2024-12-29",
             "rate": "6.14"
         },
         {
             "city": "Luknow (CC)",
+            "state": "Uttar Pradesh",
             "date": "2024-12-29",
             "rate": "6.53"
         },
         {
             "city": "Muzaffurpur (CC)",
+            "state": "Bihar",
             "date": "2024-12-29",
             "rate": "6.60"
         },
         {
             "city": "Nagpur",
+            "state": "Maharashtra",
             "date": "2024-12-29",
             "rate": "5.80"
         },
         {
             "city": "Patna",
+            "state": "Bihar",
             "date": "2024-12-29",
             "rate": "6.60"
         },
         {
             "city": "Ranchi  (CC)",
+            "state": "Jharkhand",
             "date": "2024-12-29",
             "rate": "6.48"
         },
         {
             "city": "Varanasi (CC)",
+            "state": "Uttar Pradesh",
             "date": "2024-12-29",
             "rate": "6.33"
         }
