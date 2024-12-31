852197e35270525f23407641cd366a83dbdecd75
ReaperOAK <136883103+ReaperOAK@users.noreply.github.com>
2024-12-31 14:22:47 +0000

    Build: (e50e963) update

---
+++ ads.js
@@ -0,0 +1,22 @@
+// Function to load the ad script
+function loadAdScript() {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl25418255.profitablecpmrate.com/93/93/27/93932799bb83ca32cab397e9f7902b9f.js';
+    document.body.appendChild(script);
+  }
+  
+  // Function to check and control ad display frequency
+  function controlAdDisplay() {
+    const lastAdDisplay = localStorage.getItem('lastAdDisplay');
+    const now = new Date().getTime();
+    const adDisplayInterval = 1 * 1 * 1 * 1000; // 1s in milliseconds
+  
+    if (!lastAdDisplay || now - lastAdDisplay > adDisplayInterval) {
+      loadAdScript();
+      localStorage.setItem('lastAdDisplay', now);
+    }
+  }
+  
+  // Call the function to control ad display
+  controlAdDisplay();
