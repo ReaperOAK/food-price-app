b99c874e6306bb8dd4984ce52a5708e92525a222
Lokesh Singh <lokeshsinghsodha28@gmail.com>
2025-01-12 18:57:16 +0530

    Blog Added - Barwala

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,11 +1,19 @@
 const blogs = [
   {
+    link: 'egg-rate-barwala',
+    image: '/eggrate2.jpg',
+    title: 'Egg Prices in Barwala, Hisar(Haryana)',
+    description: 'Explore the Egg Economy of Barwalas, What Makes Barwala’s Eggs Special?, Seasonal Trends and Price Fluctuations',
+    contentComponent: './blogs/egg-rate-barwala', // Path to the content component
+    uploadDate: '2025-01-11',
+  },
+  {
     link: 'blog-1',
     image: '/desiegg.jpg',
     title: 'Understanding Today\'s Egg Rates Across Major Indian Cities',
     description: 'Explore the current egg rates in various cities, including Barwala, Hyderabad, Namakkal, Delhi, Mumbai, and more.',
     contentComponent: './blogs/blog-1', // Path to the content component
-    uploadDate: '2023-10-01',
+    uploadDate: '2024-10-01',
   },
   {
     link: 'blog-2',
@@ -13,7 +21,7 @@ const blogs = [
     title: 'Understanding Egg Rates in India',
     description: 'A comprehensive guide to understanding the factors that influence egg prices in India.',
     contentComponent: './blogs/blog-2',
-    uploadDate: '2023-10-03',
+    uploadDate: '2024-10-03',
   },
   // Add more blog objects as needed
 ];
