456670a0310d8b4f23c1bd4344a38262bcf6e50a
ReaperOAK <136883103+ReaperOAK@users.noreply.github.com>
2025-04-23 14:44:10 +0000

    Build: (67653ee) check

--- php/webstories/generate_webstories_sitemap.php
+++ php/webstories/generate_webstories_sitemap.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Database connection
-include dirname(__DIR__) . '/config/db.php';
+include_once dirname(__DIR__) . '/config/db.php';
 
 // Configuration with absolute paths
 $basePath = dirname(dirname(dirname(__FILE__))); // Go up two levels from webstories dir
